999b8f302316bf865e3a9f3eb4c3574fb639614d
Guggilam Leela Naga Sai Sri Saketh <guggilamsaketh@gmail.com>
2025-08-02 08:34:47 +0530

    Create Palindrome Number.java

---
+++ Easy/Palindrome Number.java
@@ -0,0 +1,18 @@
+// Problem: Palindrome Number
+// Link: https://leetcode.com/problems/palindrome-number/
+// Difficulty: Easy
+class Solution {
+    public boolean isPalindrome(int x) {
+        int digit,reversed=0,dup=x;
+        while(x>0){
+            digit = x%10;
+            reversed = reversed*10+digit;
+            x=x/10;
+        }
+        if(dup==reversed){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
